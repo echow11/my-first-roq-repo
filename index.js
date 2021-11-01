97548f47abba13569acb548985d0680a34ce12fc
James Walsh <james.jwalsh@outlook.com>
2021-11-01 14:27:19 +0000

    Completion of different calculations

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-//Test comment
+const  addition = (numberOne, numberTwo) => {
+    let total = numberOne + numberTwo;
+    console.log(`The result of ${numberOne} + ${numberTwo} equals ${total}!`);
+} 
+
+const  subtraction = (numberOne, numberTwo) => {
+    let total = numberOne - numberTwo;
+    console.log(`The result of ${numberOne} - ${numberTwo} equals ${total}!`);
+} 
+
+const  multiply = (numberOne, numberTwo) => {
+    let total = numberOne * numberTwo;
+    console.log(`The result of ${numberOne} * ${numberTwo} equals ${total}!`);
+} 
+
+const  divide = (numberOne, numberTwo) => {
+    let total = numberOne / numberTwo;
+    console.log(`The result of ${numberOne} / ${numberTwo} equals ${total}!`);
+} 
+
+
+addition(10, 5);
+subtraction(11, 6);
+multiply(12, 7);
+divide(14, 2);
